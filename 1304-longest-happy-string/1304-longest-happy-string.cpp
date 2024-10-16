9da8658d888e6017d7b4fec3925d3047e973b5ee
Nihaal SP <150607136+nerdylelouch@users.noreply.github.com>
2024-10-17 00:06:48 +0530

    Time: 0 ms (100%), Space: 7.5 MB (97.08%) - LeetHub

--- 1304-longest-happy-string/1304-longest-happy-string.cpp
+++ 1304-longest-happy-string/1304-longest-happy-string.cpp
@@ -3,7 +3,7 @@ public:
     string longestDiverseString(int a, int b, int c) {
         int currA = 0, currB = 0, currC = 0;
         int maxLen = a + b + c, i = 0;
-        string result;
+        string result; 
         
         while (i < maxLen) {
             if ((currA != 2 && a >= b && a >= c) || (a > 0 && (currB == 2 || currC == 2))) {
