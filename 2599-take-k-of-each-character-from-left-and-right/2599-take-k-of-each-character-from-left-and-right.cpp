7cee66db985a3954082b35242b24638a481e52fd
Nihaal SP <150607136+nerdylua@users.noreply.github.com>
2024-11-20 23:15:11 +0530

    Time: 17 ms (52.68%), Space: 11.9 MB (92.24%) - LeetHub

---
+++ 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int takeCharacters(string s, int k) {
+        int n=s.size();
+        if(!k) return 0;
+        int arr[]={0,0,0},ans=0;;
+        for(auto x:s) arr[x-'a']++;
+        if(arr[0]<k || arr[1]<k || arr[2]<k) return -1;
+        if((arr[0]+arr[1]+arr[2])==k*3) return n;
+        int i=0,j=0;
+        while(j<n){
+            arr[s[j]-'a']--;
+            while((arr[0]<k || arr[1]<k || arr[2]<k) && i<j){
+                arr[s[i]-'a']++;
+                i++;
+            }
+            ans=max(ans,j-i+1);
+           j++;
+        }
+        return n-ans;
+    }
+};
