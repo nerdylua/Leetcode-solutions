ad8b36b9688a2a0fd52b4b6a8bd4c654d7e3931b
Nihaal SP <150607136+nerdylua@users.noreply.github.com>
2024-11-29 00:04:00 +0530

    Time: 253 ms (75%), Space: 108.8 MB (70.38%) - LeetHub

--- 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.cpp
@@ -1,42 +1,54 @@
 class Solution {
 public:
+
+    typedef pair<int, int> P;
+    bool isValid(int row, int col, int n, int m){
+        
+        if(row < 0 || row >= n || col < 0 || col >= m){
+            return false;
+        }
+
+        return true;
+    }
+
     int minimumObstacles(vector<vector<int>>& grid) {
-        priority_queue< pair<int, pair<int, int>>,
-                       vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>>pq;
+        
+        vector<vector<int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>>vis(n+1,vector<int>(m+1,0));
-        int dx[4] = {0, 0, 1, -1};
-        int dy[4] = {1, -1, 0, 0};
+        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        priority_queue<pair<int, P>, vector<pair<int, P>>,greater<pair<int, P>>> q;
 
-        pq.push({0, {0, 0}});
+        q.push({grid[0][0], {0, 0}});
 
-        int ans = 1e9;
-        while (!pq.empty()) {
-            auto top = pq.top();
-            int x = top.second.first;
-            int y = top.second.second;
-            int step = top.first;
-            pq.pop();
-               if (x == n - 1 and y == m - 1) {
-                  return step;
+        dist[0][0] = grid[0][0];
+
+        while(!q.empty()){
+
+            int steps = q.top().first;
+            int x     = q.top().second.first;
+            int y     = q.top().second.second;
+            q.pop();
+
+            if(x == n-1 && y == m-1){
+                return steps;
+            }
+
+            for(auto &dir: directions){
+
+                int new_x = x + dir[0];
+                int new_y = y + dir[1];
+
+                if(isValid(new_x, new_y, n, m) && dist[new_x][new_y] > steps + grid[new_x][new_y]){
+                    dist[new_x][new_y] = steps + grid[new_x][new_y];
+
+                    q.push({dist[new_x][new_y], {new_x, new_y}});
                 }
-             
-            for (int i = 0; i < 4; i++) {
-                int new_x = x + dx[i];
-                int new_y = y + dy[i];
-                
-             
-                if ( new_x >= 0 and new_x < n and new_y >= 0 and new_y < m && !vis[new_x][new_y] ) {
-                    vis[new_x][new_y]=1;
-                    if (grid[new_x][new_y] == 0) {
-                        pq.push({step, {new_x, new_y}});
-                    } else {
-                        pq.push({step + 1, {new_x, new_y}});
-                    }
-                }
+
             }
         }
-        return ans;
+
+        return -1;
     }
 };
