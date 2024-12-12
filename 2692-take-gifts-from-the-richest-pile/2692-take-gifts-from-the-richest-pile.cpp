ab435795d8d0fe5126cddbb485530378719601cb
Nihaal SP <150607136+nerdylua@users.noreply.github.com>
2024-12-12 12:47:29 +0530

    Time: 0 ms (100%), Space: 13.4 MB (16.26%) - LeetHub

---
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        priority_queue<int> maxheap(gifts.begin(), gifts.end());
+        for(int i =0; i < k; ++i){
+            int n = maxheap.top();
+            maxheap.pop();
+            maxheap.push(floor(sqrt(n)));
+        }
+        double total = 0;
+        while (!maxheap.empty()) {
+        total += maxheap.top();
+        maxheap.pop();
+        }
+        return total;
+    }
+};
