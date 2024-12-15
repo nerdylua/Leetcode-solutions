e626309be03d2ccaa151d1096e297e87bd792732
Nihaal SP <150607136+nerdylua@users.noreply.github.com>
2024-12-15 14:47:56 +0530

    Time: 0 ms (100%), Space: 26.6 MB (100%) - LeetHub

--- 3632-button-with-longest-push-time/3632-button-with-longest-push-time.cpp
+++ 3632-button-with-longest-push-time/3632-button-with-longest-push-time.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int buttonWithLongestTime(vector<vector<int>>& events) {
-        int longestTime = events[0][1];
-        int longestButton = events[0][0];
-        for (int i = 1; i < events.size(); ++i) {
-        int duration = events[i][1] - events[i - 1][1];
-
-        if (duration > longestTime || (duration == longestTime && events[i][0] < longestButton)) {
-            longestTime = duration;
-            longestButton = events[i][0];
+        int smalli = events[0][0];
+        int maxi = events[0][1];
+        for (int i = 1; i < events.size(); i++){
+            int totaltime = events[i][1] - events[i-1][1]; // calc the time diff
+            if (totaltime > maxi || (totaltime == maxi && events[i][0] < smalli)){
+                maxi = totaltime;
+                smalli = events[i][0];
+            }   
         }
-    }
-       return longestButton; 
+        return smalli;     
     }
 };
