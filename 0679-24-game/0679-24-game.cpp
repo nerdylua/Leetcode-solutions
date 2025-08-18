d356dd4ae1df6eafa5aafe3f242e1e4a9e3081ed
Nihaal SP <150607136+nerdylua@users.noreply.github.com>
2025-08-18 06:51:43 +0530

    Time: 63 ms (20.48%), Space: 28.2 MB (23.13%) - LeetHub

---
+++ 0679-24-game/0679-24-game.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool judgePoint24(vector<int>& cards) {
+        vector<double> nums(cards.begin(), cards.end());
+        return dfs(nums);
+    }
+private:
+    static constexpr double EPS = 1e-6;
+    bool dfs(vector<double>& nums) {
+        int n = nums.size();
+        if (n == 1) return fabs(nums[0] - 24.0) < EPS;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+
+                vector<double> next;
+                for (int k = 0; k < n; k++) 
+                    if (k != i && k != j) next.push_back(nums[k]);
+                for (double res : compute(nums[i], nums[j], i < j)) {
+                    next.push_back(res);
+                    if (dfs(next)) return true;
+                    next.pop_back();
+                }
+            }
+        }
+        return false;
+    }
+
+    vector<double> compute(double a, double b, bool unique) {
+        vector<double> results;
+        if (unique) {                  
+            results.push_back(a + b);
+            results.push_back(a * b);
+        }
+        results.push_back(a - b);
+        results.push_back(b - a);
+        if (fabs(b) > EPS) results.push_back(a / b);
+        if (fabs(a) > EPS) results.push_back(b / a);
+        return results;
+    }
+};
